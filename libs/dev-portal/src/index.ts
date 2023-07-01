143e87ee8ce826d4de79b7b74ada13ddb38d6a09
Vijay Pushkin <vijaypushkin.a@gmail.com>
2023-07-01 16:54:48 +0530

    chore: moved dev-portal to libs/

---
+++ libs/dev-portal/src/index.ts
@@ -0,0 +1,1 @@
+export { devPortal } from './lib/dev-portal';
---
+++ libs/dev-portal/src/lib/dev-portal.spec.ts
@@ -0,0 +1,7 @@
+import { devPortal } from './dev-portal';
+
+describe('devPortal', () => {
+  it('should work', () => {
+    expect(devPortal()).toEqual('dev-portal');
+  });
+});
